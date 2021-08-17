5eb8cce7712e94ae03852e0b6ddbea745d9f4549
Max Ivanskyi <m.ivanskiy@gmail.com>
2021-08-17 08:51:22 +0300

    HM 1

---
+++ main.js
@@ -0,0 +1,16 @@
+// HM task 1 (return generated login 2-20 symbols)
+
+
+// HM task 2 (return generated password 8-12 symbols) 
+
+
+// HM task 3 (mix array function)
+const sourceArray = [1, 2, 3, 4, 5];
+
+function mixArray(sourceArray){
+    let mixedArray = sourceArray;
+
+    return mixedArray;
+};
+console.log(m1);
+console.log(mixArray(sourceArray));
