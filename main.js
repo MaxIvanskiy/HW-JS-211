19426d6499b96435aa768f0ea347610f2fde283c
Max Ivanskyi <m.ivanskiy@gmail.com>
2021-08-18 11:44:48 +0300

    HW2 tasks 1 2

--- main.js
+++ main.js
@@ -83,5 +83,40 @@ const convertNum = (num) => {
     return result;
 }
 
-const conNum = convertNum(26);
+const conNum = convertNum(20);
 // console.log(conNum);
+
+// HM2 task 2
+const tempConvert = (num, from, to) => {
+    let result = 0;
+
+    let convert = `${from}${to}`;
+    console.log(convert);
+
+    const xc = 32;
+    const yc = 1.8;
+    const xk = 459.67;
+    const yk = 1.8;
+    const zk = 273.15;
+
+    if(convert == 'fc'){
+        result = (num - xc) / yc;
+    }else if(convert == 'cf'){
+        result = num * yc + xc;
+    }else if(convert == 'fk'){
+        result = (num + xk) / yk;
+    }else if(convert == 'kf'){
+        result = num * yk - xk;
+    }
+    else if(convert == 'ck'){
+        result = num + zk;
+    }else if(convert == 'kc'){
+        result = num - zk;
+    }else{
+        result = false;
+    }
+    
+    return result;
+}
+let conv = tempConvert(100, 'c', 'f');
+console.log(conv);
