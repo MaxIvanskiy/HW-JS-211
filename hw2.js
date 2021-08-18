6448efb9e9947b729ab868886089d221f02e4fd3
Max Ivanskyi <m.ivanskiy@gmail.com>
2021-08-18 17:41:06 +0300

    HW2

--- hw2.js
+++ hw2.js
@@ -1,5 +1,5 @@
-// ----------------- HW2 ------------------
-// task 1
+// // ----------------- HW2 ------------------
+// // task 1
 const numeralCodes = [['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'],
                         ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC'],
                         ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM']];
@@ -17,12 +17,11 @@ const convertNum = (num) => {
 const conNum = convertNum(20);
 // console.log(conNum);
 
-// task 2
+// // task 2
 const tempConvert = (num, from, to) => {
     let result = 0;
 
     let convert = `${from}${to}`;
-    console.log(convert);
 
     const xc = 32;
     const yc = 1.8;
@@ -38,8 +37,7 @@ const tempConvert = (num, from, to) => {
         result = (num + xk) / yk;
     }else if(convert == 'kf'){
         result = num * yk - xk;
-    }
-    else if(convert == 'ck'){
+    }else if(convert == 'ck'){
         result = num + zk;
     }else if(convert == 'kc'){
         result = num - zk;
@@ -50,4 +48,39 @@ const tempConvert = (num, from, to) => {
     return result;
 }
 let conv = tempConvert(100, 'c', 'f');
-console.log(conv);
+// console.log(conv);
+
+
+// task 1 copy from VladBraga
+function convert(num) { 
+    if(num < 1){ return "";}
+    if(num >= 1000){ return "M" + convert(num - 1000);}
+    if(num >= 900){ return "CM" + convert(num - 900);}
+    if(num >= 500){ return "D" + convert(num - 500);}
+    if(num >= 400){ return "CD" + convert(num - 400);}  
+    if(num >= 100){ return "C" + convert(num - 100);}
+    if(num >= 90){ return "XC" + convert(num - 90);}
+    if(num >= 50){ return "L" + convert(num - 50);}
+    if(num >= 40){ return "XL" + convert(num - 40);}
+    if(num >= 10){ return "X" + convert(num - 10);}
+    if(num >= 9){ return "IX" + convert(num - 9);}
+    if(num >= 5){ return "V" + convert(num - 5);}
+    if(num >= 4){ return "IV" + convert(num - 4);}
+    if(num >= 1){ return "I" + convert(num - 1);}  
+  }
+
+//   console.log(convert(10));
+
+// task 2 copy from Daria Frantsevich
+const convertValue = (value, from, to) => { 
+  
+    const condition = {
+      cf: value => (value * 9 / 5) + 32, 
+      ck: value => value + 273.15, 
+      fc: value => (value - 32) * 5 / 9, 
+      kc: value => value - 273.15
+    }   
+    return condition[from + to]?.(value);
+  } 
+
+  // console.log(convertValue(10, 'c', 'f'));
